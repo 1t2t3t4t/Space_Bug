89af7575596e0cdc067f953dbe04dadf0659c13e
Boss <bossnathakorn@gmail.com>
2021-11-06 22:06:31 +0700

    add player basic model and animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     public float Speed = 10;
     public float Jump = 10;
+    public float TurnSpeed = 3;
+
+    public Animator PlayerAnimator;
+    public Transform ModelTransform;
 
     private Camera _camera;
     private CharacterController _characterController;
     private float _yVelocity = 0;
+    
+    private static readonly int IsMoving = Animator.StringToHash("isMoving");
 
     private void Start()
     {
@@ -41,10 +47,27 @@ public class PlayerController : MonoBehaviour
                 break;
         }
 
-        var movementDir = transform.TransformDirection(CalculateMovementDirection());
+        var movement = CalculateMovementDirection();
+        var movementDir = transform.TransformDirection(movement);
         if (movementDir != Vector3.zero)
         {
             _characterController.Move(movementDir * Time.deltaTime);
         }
+        TurnPlayer();
+        UpdateAnimationState(movement);
+    }
+
+    private void TurnPlayer()
+    {
+        var forwardDir = _camera.transform.forward;
+        var targetRotation = Quaternion.FromToRotation(Vector3.forward, forwardDir);
+        targetRotation.x = 0;
+        ModelTransform.rotation = Quaternion.Slerp(ModelTransform.rotation, targetRotation, Time.deltaTime * TurnSpeed);
+    }
+
+    private void UpdateAnimationState(Vector3 movement)
+    {
+        var isMoving = movement.x != 0 || movement.z != 0;
+        PlayerAnimator.SetBool(IsMoving, isMoving);
     }
 }
