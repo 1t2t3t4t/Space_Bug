b60ae7c34a00cfb06b9f80cdd4a5d3fc884a6df8
Boss <bossnathakorn@gmail.com>
2021-11-07 20:46:55 +0700

    impl basic pistol shoot

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,15 +1,39 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    public GameObject BulletPrefab;
     public ParticleSystem GunFlash;
+
+    [SerializeField]
+    private Weapon CurrentWeapon;
+    
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
 
     private void Update()
     {
+        var aimDir = _camera.transform.forward;
         if (Input.GetMouseButtonDown(0))
         {
-            GunFlash.Play();
+            Fire(aimDir);
         }
+        Debug.DrawRay(GunFlash.transform.position, aimDir, Color.red);
+    }
+
+    private void Fire(Vector3 aimDir)
+    {
+        var bullet = Instantiate(BulletPrefab);
+        bullet.transform.position = GunFlash.transform.position;
+        bullet.transform.rotation = Quaternion.LookRotation(aimDir);
+        Debug.Log(aimDir.normalized);
+        bullet.GetComponent<Rigidbody>().velocity = aimDir.normalized * CurrentWeapon.BulletSpeed;
+        GunFlash.Play();
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,8 @@
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Create Weapon")]
+public class Weapon : ScriptableObject
+{
+    public float BulletSpeed;
+}
